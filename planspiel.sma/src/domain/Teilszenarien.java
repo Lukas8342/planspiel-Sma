0327c1fff4fc98468c1e837bb205122d36282a86
Lukas8342 <122049965+Lukas8342@users.noreply.github.com>
2023-02-09 17:49:55 +0100

    Create Teilszenarien.java
    Teilszenarien

---
+++ planspiel.sma/src/domain/Teilszenarien.java
@@ -0,0 +1,76 @@
+package domain;
+
+public class Teilszenarien {
+	String[] szen1 = {
+			"Es stellt sich heraus, dass die Vertraulichkeit der Daten bei dem Angriff nicht ordnungsgemäß\r\n"
+					+ "gewahrt wurde, da einige Mitarbeitende unachtsam mit den Kundendaten umgegangen sind und\r\n"
+					+ "diese an Dritte weitergegeben haben. Das führt dazu, dass die Kunden, Mitarbeitenden und\r\n"
+					+ "Lieferanten noch mehr verunsichert sind, da sie nicht wissen, wer alles Zugriff auf ihre Daten hatte.\r\n",
+			"Viele Kunden haben Angst, dass durch weitere Hackerangriffe Daten verloren gehen oder verändert\r\n"
+					+ "werden und dadurch weitere Störungen oder Kosten für sie entstehen könnten.",
+			"Aus den getroffenen Entscheidungen wurde der Entschluss gezogen, die IT-Systeme des\r\n"
+					+ "Unternehmens vorerst herunterzufahren, um weitere Angriffe zu verhindern. Die Website mit dem\r\n"
+					+ "Kundenportal ist jetzt nicht mehr erreichbar. Kunden beschweren sich und auch Online-Bestellungen\r\n"
+					+ "sind nicht mehr möglich.",
+			"Der laufende Betrieb der Supermärkte ist immer noch nicht auf dem alten Stand. Die letzten\r\n"
+					+ "Entscheidungen haben das Unternehmen viel Geld gekostet. Einzelne Filialen stehen kurz vor der\r\n"
+					+ "Schließung.\r\n" + "",
+			"Durch ihre vorherigen Entscheidungen ist die öffentliche Wahrnehmung auf ihr Unternehmen\r\n"
+					+ "gesunken. In den sozialen Medien wird schlechtes über ihre Supermarktkette verbreitet. Kunden\r\n"
+					+ "drohen zu anderen Supermarktketten zu wechseln. Negative finanzielle Folgen stehen bevor." };
+
+	String[] szen2 = {
+			"Der Hack bewirkte, dass die für unsere Produktion notwendige Software für drei Tage lang nicht\r\n"
+					+ "verfügbar war. Es wurden – laut Hersteller - keine Daten entwendet, verändert und es seien keine\r\n"
+					+ "Daten verloren gegangen.",
+			"Nachdem die Systeme erfolgreich und sehr schnell vom Softwarehersteller innerhalb eines Tages\r\n"
+					+ "wiederhergestellt wurden fällt uns auf, dass Daten von einer Woche fehlen.",
+			"Zwei Tage, nachdem der Hersteller Entwarnung gegeben hat, schlägt unser Sicherheitssystem Alarm.\r\n"
+					+ "Von mehreren Maschinen und Rechnern würden gleichzeitig Verbindungen zu verdächtigen Servern\r\n"
+					+ "stattfinden, die auf einer C&C-Server Liste stehen.",
+			"Wenige Stunden, nachdem vom Hersteller die Warnung kam, quillt das Postfach vom Ticketsystem\r\n"
+					+ "über, über 20 Benutzer melden, dass sie nicht mehr arbeiten können, auf dem Desktop sind\r\n"
+					+ "kryptische Dateien aufgetaucht und dass eine bedrohliche Erpressermeldung auf deren Bildschirm zu\r\n"
+					+ "sehen war",
+			"Die Software war nach wenigen Stunden wieder einsatzbereit. Es ist kein Datenverlust aufgetreten\r\n"
+					+ "und somit ist kein echter Schaden entstanden. Nach ca. 8 Wochen werden wir von einem\r\n"
+					+ "chinesischen Kunden benachrichtigt, dass es sehr nett von uns gewesen sei, ihm unsere Unterlagen\r\n"
+					+ "für unsere neue Technologie zukommen zu lassen. Da in den Unterlagen allerdings abfällige und\r\n"
+					+ "beleidigende Kommentare ihm gegenüber stehen, hat er sich entschieden, ein Patent für diese\r\n"
+					+ "Technologie anzumelden und sie auf dem asiatischen Markt selbst zu vermarkten." };
+	String[] szen3 = {
+			"Ihre Kunden haben große Bedenken und beschweren sich, weil sie Angst vor weiteren Hackerangriffen auf das Unternehmen haben und befürchten, dass ihre persönlichen Daten preisgegeben oder veröffentlicht werden könnten.",
+			"Viele Kunden haben Angst, dass durch weitere Hackerangriffe Daten verloren gehen oder verändert werden und dadurch weitere Störungen oder Kosten für sie entstehen könnten.",
+			"Aufgrund des Ausfalls ihrer Anlagen besteht das Risiko von Stromausfällen, insbesondere bei Kunden in ländlichen Gegenden, die über eine schlechtere Netzanbindung und fehlende Ersatzversorgungsmöglichkeiten verfügen.",
+			"Aufgrund des andauernden Ausfalls der Windkraftanlagen und der kurzfristigen Übernahme von Konkurrenzunternehmen in der Versorgung ihrer Kunden, droht es dem Unternehmen, dass die Umsatzziele für dieses Quartal nicht erreicht werden",
+			"Das Vertrauen der Öffentlichkeit in das Unternehmen sinkt aufgrund der vielen Berichte über den Vorfall in den Medien. Viele Kunden drohen, zu Konkurrenzunternehmen zu wechseln, was das Risiko von Rückgängen bei den Umsatzzahlen erhöhen könnte." };
+	String[] szen4 = { "", "", "", "", "" };
+	String[] szen5 = {
+			"Ihre Kunden haben Bedenken in der Sicherheit ihrer persönlichen Daten und befürchten die\r\n"
+					+ "Veröffentlichungen oder einen eventuellen Identitätsdiebstahl. ",
+			"Die Integrität der Daten wird von ihren Kunden in fragegestellt, es herrscht eine generelle\r\n"
+					+ "Unsicherheit der Kunden über die Auswirkung des Cyberangriffes. ",
+			"In ihrem Kundenstamme herrscht Verwirrung über die Auslieferungen ihrer Bestellungen. ",
+			". Ein Teil ihrer Kunden rechnet mit dem Verlust von Kapital durch nicht Erfüllung von\r\n"
+					+ "Lieferterminen und drohen mit dem Rückzug ihrer Aufträge.",
+			"Der Verlust in Millionenhöhe rückt das Unternehmen in ein schlechtes Licht. Die\r\n"
+					+ "Öffentlichkeit ist der Meinung das ein solcher Vorfall auf das Handel in der Führungsebne\r\n"
+					+ "zurückzuverfolgen ist." };
+
+	public String[] getScenario(int id) {
+		switch (id) {
+		case 0: // szen 1
+			return szen1;
+		case 5: // szen 2
+			return szen2;
+		case 10: // szen3
+			return szen3;
+		case 15:// szen 4
+			return szen4;
+		case 20: // szen 5
+			return szen5;
+		}
+		return null;
+	}
+
+}
