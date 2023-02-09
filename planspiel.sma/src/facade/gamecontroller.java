52e46ca9f9e69a8877f33da217aca49933ddfee2
Lukas8342 <122049965+Lukas8342@users.noreply.github.com>
2023-02-09 17:51:23 +0100

    Update gamecontroller.java
    a

--- planspiel.sma/src/facade/gamecontroller.java
+++ planspiel.sma/src/facade/gamecontroller.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 
 import domain.Roles;
 import domain.Rolle;
+import domain.Teilszenarien;
 
 public class gamecontroller {
 	Roles roles = new Roles();
+	Teilszenarien szen = new Teilszenarien();
 
 	public void asignRoles(String name, int i) {
 		roles.asign(i, name);
@@ -18,6 +20,10 @@ public class gamecontroller {
 
 	public void createRoles() {
 		roles.createAllRoles();
+	}
+
+	public String[] getScenario(int id) {
+		return szen.getScenario(id);
 	}
 
 }
